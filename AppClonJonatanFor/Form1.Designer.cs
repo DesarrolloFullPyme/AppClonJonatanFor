30402f54c22288593d200ccfc857f59ddc5b1556
desarrolloFullPyme <desarrollo@fullpyme.cl>
2022-01-31 12:14:03 -0300

    Se agregan un group box por integrante

--- AppClonJonatanFor/Form1.Designer.cs
+++ AppClonJonatanFor/Form1.Designer.cs
@@ -30,9 +30,12 @@ namespace AppClonJonatanFor
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
             this.lblMensaje = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -49,15 +52,14 @@ namespace AppClonJonatanFor
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Grupo inicial";
             // 
-            // button2
+            // lblMensaje
             // 
-            this.button2.Location = new System.Drawing.Point(170, 52);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(141, 37);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "Mostrar mensaje";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Location = new System.Drawing.Point(26, 111);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(22, 21);
+            this.lblMensaje.TabIndex = 2;
+            this.lblMensaje.Text = "....";
             // 
             // label2
             // 
@@ -68,20 +70,51 @@ namespace AppClonJonatanFor
             this.label2.TabIndex = 1;
             this.label2.Text = "Aprete el boton";
             // 
-            // lblMensaje
+            // button2
             // 
-            this.lblMensaje.AutoSize = true;
-            this.lblMensaje.Location = new System.Drawing.Point(26, 111);
-            this.lblMensaje.Name = "lblMensaje";
-            this.lblMensaje.Size = new System.Drawing.Size(22, 21);
-            this.lblMensaje.TabIndex = 2;
-            this.lblMensaje.Text = "....";
+            this.button2.Location = new System.Drawing.Point(170, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 37);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Mostrar mensaje";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(12, 192);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(336, 151);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Funciones Jona";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(354, 192);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(338, 151);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Funciones Vale";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Location = new System.Drawing.Point(698, 192);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(338, 151);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Funciones Ceci";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(359, 304);
+            this.ClientSize = new System.Drawing.Size(1034, 471);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Proyecto prueba";
@@ -96,6 +129,9 @@ namespace AppClonJonatanFor
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
     }
 }
 
